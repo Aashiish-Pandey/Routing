43defb306092c6cb5bf9b322616bdd3057b826d6
Aashiish-Pandey <ash006pandey@gmail.com>
2021-03-22 15:46:02 +0530

    Rendering component for Routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,34 +1,33 @@
-import React, { Component } from 'react';
-import axios from '../../axios';
-import './Blog.css';
-import Posts from '../Posts/Posts'
-import {Route} from 'react-router-dom'
+import React, { Component } from "react";
+import axios from "../../axios";
+import "./Blog.css";
+import Posts from "../Posts/Posts";
+import { Route } from "react-router-dom";
 
 class Blog extends Component {
-    
-    render () {
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/new-post">New Post</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
 
+        {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
 
-        return (
-            <div className = "Blog">
-                <header>
-                    <nav>
-                        <ul>
-                            <li><a href = "/">Home</a></li>
-                            <li><a href = "/new-post">New Post</a></li>
-    
-                        </ul>
-                    </nav>
-                </header>
-                
-                <Route path = "/" exact render ={()=> <h1>Home</h1>} />
-            </div>
-        );
-        
-        }
+        <Route path="/" component = {Posts} />
 
-        
-    }
-
+      </div>
+    );
+  }
+}
 
 export default Blog;
